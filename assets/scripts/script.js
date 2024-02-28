1a4f0aaf2990d81f555c35952d8e6b5b7a8cdd79
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-02-28 17:34:12 +0100

    pariOdispari done

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -24,9 +24,95 @@ function controlPalindromy() /*L'INPUT NON VIENE RICEVUTO*/
     
     if (splitStr == reverseStr) {
         document.getElementById('#rispostaPalindromia').innerHTML = 
-        `La parola inserita è palindroma`;
+        `<p> La parola inserita è palindroma <p>`;
     } else {
         document.getElementById('#rispostaPalindromia').innerHTML = 
-        `La parola inserita non è palindroma`;
+        ` <p> La parola inserita non è palindroma <p>`;
+    }
+}
+
+
+/* PARI E DISPARI */
+
+const inputPD = document.querySelector(`.inputPD`);
+const btnPD = document.querySelector(`.btnPD`);
+
+const btnPari = document.querySelector(`.btnPari`);
+const btnDispari = document.querySelector(`.btnDispari`);
+
+let pari = 0;
+let dispari = 0;
+
+btnPari.addEventListener('click',plusPari())
+btnDispari.addEventListener(`click`,plusDispari()) 
+
+
+btnPD.addEventListener('click',function() 
+{
+    let inputNum = parseInt(inputPD.value);
+    console.log(`Il numero inserito è ` + inputNum);
+
+    if ((inputNum < 1) || (inputNum > 5)) {
+        document.getElementById('#rispostaPD').innerHTML = 
+        ` <p> Non hai inserito un numero compreso tra 1 e 5 <p>`;
+    } 
+    else 
+    { 
+        let randNum1To5 = getRandomInt(1,5);
+        const sumNum = inputNum + randNum1To5;
+
+        if (dispariOPari(sumNum) == `pari`) 
+        {
+            if (pari == 1) {
+                document.getElementById('#rispostaPD').innerHTML = 
+                `<p> Hai vinto, la somma è pari`
+            }
+            else if (pari == 0) {
+                document.getElementById('#rispostaPD').innerHTML = 
+                `<p> Hai perso, la somma è dispari`
+            }
+        } else if (dispariOPari(sumNum) == `dispari`)
+        {
+            if (dispari == 1) {
+                document.getElementById('#rispostaPD').innerHTML = 
+                `<p> Hai vinto, la somma è dispari`
+            }
+            else if (dispari == 0) {
+                document.getElementById('#rispostaPD').innerHTML = 
+                `<p> Hai perso, la somma è pari`
+            }
+        }
+    }
+}
+)
+
+/*FUNZIONI UTILIZZATE*/
+
+function plusPari()
+{
+    pari += 1;
+}
+
+function plusDispari()
+{
+    dispari += 1;
+}
+
+function getRandomInt(min, max) 
+{
+    const minCeil = Math.ceil(min);
+    const maxFloor = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloor - minCeil + 1) + minCeil);
+}
+
+function dispariOPari(number) 
+{
+    let resto = (number % 2);
+
+    if (resto == 1) {
+        return (`dispari`)
+    } else
+    {
+        return (`pari`)
     }
 }
