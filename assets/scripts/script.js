9b71b8238540bc2bfab39c12eb58604b977aeabf
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-02-28 16:17:57 +0100

    Snack 1 done

---
+++ assets/scripts/script.js
@@ -0,0 +1,33 @@
+const inputPal = document.querySelector(`.inputPal`);
+const btnConferma = document.querySelector(`.btnConferma`);
+
+
+
+
+/* acquisito l'input, lo dividiamo in 2 (il resto non ci interessa perchè la lettera di mezzo è neutra nella definizione di palindromia) 
+e verifichiamo che la prima e l'ultima lettera siano uguali, variando il contatore fino ad arrivare al centro della parola */
+/*inizialmente l'avrei fatto come sopra ma è più comodo usare i metodi split e reverse per fare un controllo più efficiente e più leggibile*/
+
+btnConferma.addEventListener('click',controlPalindromy())
+
+
+function controlPalindromy(){
+
+    const palStr = inputPal.value;
+
+    let splitStr = palStr.split(``);
+    let reverseStr = splitStr.reverse();
+
+    console.log(`La parola inserita è` + palStr);
+    console.log(`L'array estratto è` + splitStr);
+    console.log(`L'array invertito è` + reverseStr);
+    
+    if (splitStr == reverseStr) {
+        document.getElementById('#rispostaPalindromia').innerHTML = 
+        `La parola inserita è palindroma`;
+    } else {
+        document.getElementById('#rispostaPalindromia').innerHTML = 
+        `La parola inserita non è palindroma`;
+    }
+
+}
